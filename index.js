2cc6ecab22f9a68d6fbedb84072e2d60ab155c74
Midhun <midhunmanu384@gmail.com>
2022-12-13 19:05:09 +0530

    unnecessary comments removed

--- index.js
+++ index.js
@@ -35,9 +35,6 @@ app.post('/getCheapestFood' , (req , res) => {
 
 
 app.post("/shopping" , (req , res) => {
-  // var product_name = req.body.product_name;
-  // var product_name = product_name.repalce(" " , "+");
-  // var amazon_url = `https://www.amazon.in/s?k=${product_name}`;
   res.render("view/shopping.ejs");
 
 })
@@ -46,37 +43,11 @@ app.post("/shopitem" , (req , res) => {
     var product_name = req.body.product_name_html;
     product_name = product_name.replaceAll(" " , "+");
     var url = `https://www.amazon.in/s?k=${product_name}`;
-    // var url = "https://www.amazon.in/Apple-iPhone-13-128GB-Pink/dp/B09G9FPGTN/ref=sr_1_1_sspa?crid=3IKNX9CLJTHN4&keywords=iphone+13&qid=1670659982&sprefix=%2Caps%2C254&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1";
-
-
-    // var getLinks = () => {
-    //   (async () => {
-    //     const browser = await puppeteer.launch({headless : true});
-    //     const page = await nrowser.newPage();
-    //     await page.goto(url);
-
-    //     // finding all product urls
-    //     const findLinks = await page.evaluate(() => {
-    //       const productLinks = document.querySelectorAll("a.a-size-base.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal");
-    //       var links = [];
-    //       productLinks.forEach((elements) => {
-    //         data.push(elements.innerText);
-    //       })
-    //       console.log(data);
-    //     })
-    //   })()
-    // }
-
-
-
       (async () => {
       const browser = await puppeteer.launch({headless : true});
       const page = await browser.newPage();
       await page.goto(url);
-      // const hrefs = await page.$$eval('a.a-size-base .a-link-normal .s-underline-text .s-underline-link-text .s-link-style .a-text-normal', as => as.map(a => a.href));
-      // console.log(hrefs)
-      
-// await page.evaluate('document.querySelector("span.styleNumber").getAttribute("data-Color")')
+
       const hrefs = await page.evaluate(() => {
         const href_tag = document.querySelectorAll("a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal");
         var href_array = [];
@@ -86,9 +57,6 @@ app.post("/shopitem" , (req , res) => {
         })
         return href_array;
       })
-      // const hrefs = await page.evaluate('document.querySelector("a.a-size-base.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal").getAttribute("href")');
-      // console.log(hrefs);
-
       
       // finding all prices 
       const grab = await page.evaluate(() => {
@@ -114,9 +82,7 @@ app.post("/shopitem" , (req , res) => {
       }
 
       data = data.map(Number);
-      // console.log(data);
 
-      // const arr = [10, 5, 0, 15, 30];
       data = data.sort((a , b) => {return a - b});
       if(data[0] > data[data.length - 1]) {
         data.reverse();
@@ -129,11 +95,6 @@ app.post("/shopitem" , (req , res) => {
         for(var i = 0; i < 5; i++) {
           console.log(data[i]);
         }
-      // }
-      // catch {
-      //   console.log(data[data.length - 1]);
-      // }
-
       try {
         console.log(hrefs);
         console.log(`${product_name} pricelist:\n`)
